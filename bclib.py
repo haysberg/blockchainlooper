9ef4edbe2c1222c88be4c8a9017d92bb4999da7b
Téo Le Dozo <teo@couaque.fr>
2019-11-08 10:44:18 +0000

    Handling the maximum recursion error

--- bclib.py
+++ bclib.py
@@ -9,11 +9,14 @@ if len(sys.argv) > 3 :
     printer = sys.argv[3]
 
 def bcFindTarget(hash, target, iteration):
-    if printer == "1":
-        print(hash)
+    try:
+        if printer == "1":
+            print(hash)
 
-    if(hash == target):
-        print("The target " + target + " was found after " + str(iteration) + " iterations. Hurray !")
-    else:
-        hasher.update(hash.encode("utf-8"))
-        bcFindTarget(hasher.hexdigest(), target, iteration +1)
+        if(hash == target):
+            print("The target " + target + " was found after " + str(iteration) + " iterations. Hurray !")
+        else:
+            hasher.update(hash.encode("utf-8"))
+            bcFindTarget(hasher.hexdigest(), target, iteration +1)
+    except:
+        print('It looks like we could not find the hash you were looking for. Please not that Python\'s maximum iterations is 1000.')
